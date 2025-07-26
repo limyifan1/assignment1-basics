dae5778ee4bf970179e8c4c98b4134fbc4ef82b1
Lim Yi Fan <limyifan1@gmail.com>
2025-07-27 02:13:10 +0900

    started on get_tokenizer and encode function

---
+++ cs336_basics/tokenizer.py
@@ -0,0 +1,51 @@
+from collections.abc import Iterable, Iterator
+from collections import defaultdict
+from cs336_basics.pre_tokenize import pre_tokenize
+from multiprocessing import Pool, cpu_count
+import regex as re
+
+class Tokenizer:
+    # vocab: dict[bytes, int], e.g. { b't':5}
+    # merges: set[tuple[bytes, bytes]], {(b't', b'h')}
+    # special_tokens: list[str] | None = None,
+    def __init__(self, vocab, merges, special_tokens=None):
+        self.vocab = {value: key for key, value in vocab.items()}
+        self.merges = set(merges)
+        self.special_tokens = special_tokens if special_tokens else []
+        
+    def from_files(cls, vocab_filepath, merges_filepath, special_tokens=None):
+        return cls()
+    
+    def encode(self, text: str) -> list[int]:
+        pre_tokens = self.pre_tokenize(text)
+        encoded = []
+        for pre_token_byte in pre_tokens:
+            byte_stack: list[bytes] = []
+            for next_int in pre_token_byte:
+                next_byte = bytes([next_int])
+                if byte_stack:
+                    top_byte = byte_stack[-1]
+                    byte_pair = (top_byte, next_byte)
+                    if byte_pair in self.merges:
+                        byte_stack[-1] = top_byte + next_byte
+                        continue
+                byte_stack.append(next_byte)
+            for byte_item in byte_stack:
+                encoded.append(self.vocab[byte_item])
+        return encoded
+    
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            yield self.encode(text)
+    
+    def pre_tokenize(self, text: str) -> list[bytes]:
+        escaped_special_tokens = [re.escape(token) for token in self.special_tokens] 
+        delimitedChunks = re.split("|".join(escaped_special_tokens), text)
+        pre_tokens = []
+        for delimitedChunk in delimitedChunks:
+            PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+            matches = re.finditer(PAT, delimitedChunk)
+            for match in matches:
+                text_utf = match.group().encode("utf-8")
+                pre_tokens.append(text_utf)
+        return pre_tokens
