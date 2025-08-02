1edde717cbbef520ea0da0e2dbae2aefc2ee245a
Lim Yi Fan <limyifan1@gmail.com>
2025-08-03 00:07:21 +0800

    add training script for owt and tinystories bpe tokenizer

---
+++ cs336_basics/train_bpe_owt.py
@@ -0,0 +1,20 @@
+from cs336_basics.train_bpe import train_bpe
+import json
+
+if __name__ == "__main__":
+    vocab, merges = train_bpe(
+        input_path="data/owt_train.txt",
+        vocab_size=32000,
+        special_tokens=["<|endoftext|>"],
+    )
+
+    json_safe_vocab = {str(k): v.decode("latin-1") for k, v in vocab.items()}
+
+    with open(file="owt-vocab.json", mode="w", encoding="utf-8") as f:
+        json.dump(json_safe_vocab, f)
+
+    with open(file="owt-merges.txt", mode="w", encoding="utf-8") as f:
+        for pair in merges:
+            p1 = pair[0].decode("latin-1")
+            p2 = pair[1].decode("latin-1")
+            f.write(f"{p1} {p2}\n")
---
+++ cs336_basics/train_bpe_tinystories.py
@@ -0,0 +1,20 @@
+from cs336_basics.train_bpe import train_bpe
+import json
+
+if __name__ == "__main__":
+    vocab, merges = train_bpe(
+        input_path="data/TinyStoriesV2-GPT4-train.txt",
+        vocab_size=1000,
+        special_tokens=["<|endoftext|>"],
+    )
+
+    json_safe_vocab = {str(k): v.decode("latin-1") for k, v in vocab.items()}
+
+    with open(file="tinystories-vocab.json", mode="w", encoding="utf-8") as f:
+        json.dump(json_safe_vocab, f)
+
+    with open(file="tinystories-merges.txt", mode="w", encoding="utf-8") as f:
+        for pair in merges:
+            p1 = pair[0].decode("latin-1")
+            p2 = pair[1].decode("latin-1")
+            f.write(f"{p1} {p2}\n")
