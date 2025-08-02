71c8fe6f72a695e03e829fda397063d5884721d0
Lim Yi Fan <limyifan1@gmail.com>
2025-08-03 00:44:56 +0800

    add timing calculation for training

--- cs336_basics/train_bpe_tinystories.py
+++ cs336_basics/train_bpe_tinystories.py
@@ -1,7 +1,9 @@
 from cs336_basics.train_bpe import train_bpe
 import json
+import time
 
 if __name__ == "__main__":
+    start_time = time.time()  # Record the start time
     vocab, merges = train_bpe(
         input_path="data/TinyStoriesV2-GPT4-train.txt",
         vocab_size=1000,
@@ -18,3 +20,6 @@ if __name__ == "__main__":
             p1 = pair[0].decode("latin-1")
             p2 = pair[1].decode("latin-1")
             f.write(f"{p1} {p2}\n")
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time
+    print(f"Script ran for {elapsed_time:.2f} seconds.")
